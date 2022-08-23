dd40d3f05c26808a7514b6a3e039d9c837a0cdb2
daniel salgado wsdl <daniel.salgado.p@gmail.com>
2022-08-23 11:57:12 +0200

    typos

---
+++ tests/fixture_data/company.sql
@@ -0,0 +1,15 @@
+use mivoz;
+
+INSERT INTO
+    company(name,telephone)
+VALUES
+('compañia_nombre1', '1'),
+('compañia_nombre2', '2'),
+('compañia_nombre3', '3'),
+('compañia_nombre4', '4'),
+('compañia_nombre5', '5'),
+('compañia_nombre6', '6'),
+('compañia_nombre7', '7'),
+('compañia_nombre8', '8'),
+('compañia_nombre9', '9'),
+('compañia_nombre10', '10')
---
+++ tests/fixture_data/contact.sql
@@ -0,0 +1,16 @@
+use mivoz;
+truncate table contact;
+INSERT INTO
+  contact(name, surname)
+VALUES
+  ('contact_nombre1', 'contact_apellido1'),
+  ('contact_nombre2', 'contact_apellido2'),
+  ('contact_nombre3', 'contact_apellido3'),
+  ('contact_nombre4', 'contact_apellido4'),
+  ('contact_nombre5', 'contact_apellido5'),
+  ('contact_nombre6', 'contact_apellido6'),
+  ('contact_nombre7', 'contact_apellido7'),
+  ('contact_nombre8', 'contact_apellido8'),
+  ('contact_nombre9', 'contact_apellido9'),
+  ('contact_nombre10', 'contact_apellido10')
+  ;
